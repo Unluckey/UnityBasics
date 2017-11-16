a312d35796c160a89d945dcecc00850569b2fb03
Unluckey <diczero@126.com>
2017-11-17 00:13:40 +0800

    Add new constructors & overrides of method register()

--- NotifySystem/Basic/Listener.cs
+++ NotifySystem/Basic/Listener.cs
@@ -5,7 +5,27 @@ using System;
 namespace NotifySystem{
 	[Serializable]
 	public class Listener{
+		public Dictionary<NotifyType,EventListenerDelegate> recieverDic = null;
 
+		public Listener(){
+			recieverDic = new Dictionary<NotifyType, EventListenerDelegate> ();
+		}
+		public Listener(NotifyType notifyType,EventListenerDelegate eventReceiver){
+			recieverDic = new Dictionary<NotifyType, EventListenerDelegate> ();
+			recieverDic.Add (notifyType,eventReceiver);
+			register ();
+		}
+			
+		public Listener(Dictionary<NotifyType,EventListenerDelegate> recieverDic){
+			this.recieverDic = recieverDic;
+			register ();
+		}
+
+		public void register(){
+			foreach (var Key in recieverDic.Keys) {
+				NotificationCenter.getInstance ().registerObserver (Key, recieverDic[Key]);
+			}
+		}
 		public void register(Dictionary<NotifyType,EventListenerDelegate> recieverDic){
 			foreach (var Key in recieverDic.Keys) {
 				NotificationCenter.getInstance ().registerObserver (Key, recieverDic[Key]);
