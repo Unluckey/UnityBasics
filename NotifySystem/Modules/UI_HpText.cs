48c4a33f2c0ba57d1a9f4699f224ce4a61c8c652
Unluckey <diczero@126.com>
2017-11-17 00:24:18 +0800

    First commit version. To change the UI text as hp/maxHp of target.

---
+++ NotifySystem/Modules/UI_HpText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_HpText : BaseEntity {
+	NotifySystem.Listener listener = null;
+	public Creature target = null;
+	void Awake(){
+		base.Awake ();
+
+		Dictionary<NotifySystem.NotifyType,NotifySystem.EventListenerDelegate> recieverDic = 
+			new Dictionary<NotifySystem.NotifyType, NotifySystem.EventListenerDelegate> ();
+		NotifySystem.EventListenerDelegate reciever = new NotifySystem.EventListenerDelegate (UpdateUI);
+		recieverDic.Add (NotifySystem.NotifyType.CREATURE_CHANGE_HP,reciever);
+		recieverDic.Add (NotifySystem.NotifyType.CREATURE_CHANGE_MAXHP,reciever);
+
+		listener = new NotifySystem.Listener (recieverDic);
+	}
+
+	void UpdateUI(NotifySystem.NotifyEvent notifyEvent){
+		GetComponent<Text>().text = (target.hp.ToString() + "/" + target.maxHp.ToString());
+	}
+}
