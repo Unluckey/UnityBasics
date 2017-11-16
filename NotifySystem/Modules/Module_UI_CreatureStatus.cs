951573f4b541b87258b910e17b8628aadf3506b4
Unluckey <diczero@126.com>
2017-11-17 00:22:01 +0800

    First commit version. Maybe delete later.

---
+++ NotifySystem/Modules/Module_UI_CreatureStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace NotifySystem{
+	public class Module_UI_CreatureStatus : Listener {
+		public Module_UI_CreatureStatus(){
+			
+		}
+
+		void UpdateUI(){
+			
+		}
+	}
+}
