1cb54518644bf93a2c47bd3088b603ebe53a795e
Unluckey <diczero@126.com>
2017-11-17 18:47:12 +0800

    First commite version

---
+++ SceneLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+	public static string targetSceneName;
+
+
+	AsyncOperation asyncO;
+	public Texture2D[] animations;
+	private int nowFram;
+
+	float progress = 0;
+
+	void Awake(){
+		StartCoroutine (LoadScene());
+	}
+	IEnumerator LoadScene(){
+		asyncO = SceneManager.LoadSceneAsync (targetSceneName);
+		yield return asyncO;
+	}
+
+	void OnGUI(){
+	}
+	void Update(){
+		
+		progress = asyncO.progress * 100;
+		Debug.Log("Loading:"+((int)progress).ToString()+"%");
+	}
+}
---
+++ World.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+
+public class World:MonoBehaviour
+{
+	static World instance = null;
+	public static World Instance{
+		get{
+			if(instance == null)
+				instance = new World();
+			return instance;
+		}
+		set{ }
+	}
+
+	public void ChangeScene(string targetSceneName){
+		
+		SceneLoader.targetSceneName = targetSceneName;
+		SceneManager.LoadScene ("LoadingScene");
+	}
+}
+
